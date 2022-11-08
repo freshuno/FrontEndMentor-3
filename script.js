0bba6d709a2f0d8fe2ceb2319093bc45ee37d0a8
Freshek <81330932+freshuno@users.noreply.github.com>
2022-11-08 21:38:21 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ var plus = document.getElementById("buttonplus");
 var bigphoto = document.getElementById("bigphoto");
 const cartbutton = document.getElementById("cart");
 var bigpicture = document.getElementById("bigpicture");
+var closebutton = document.getElementById("close");
+const smallPhotos = document.querySelectorAll(".smallphoto");
 var amount = 0;
-/* tego jednak nie trzeba 
 bigphoto.addEventListener("click", function(){
      bigpicture.style.display = "block";
-}) */
+})
+closebutton.addEventListener("click", function(){
+    bigpicture.style.display ="none";
+})
 plus.addEventListener("click", function(){
     amount = amount + 1;
     document.getElementById("buttonp").innerHTML = amount;
@@ -22,3 +26,16 @@ minus.addEventListener("click", function(){
 cartbutton.addEventListener("click", function(){
     document.querySelector(".cartmenu").classList.toggle("cartdisplay");
 })
+//ZMIENIANIE FOCUSU NA SMALLPHOTO
+ smallPhotos.forEach(link => {
+    link.addEventListener("click", () => {
+        smallPhotos.forEach(link1 => {
+            if (link1.classList.contains('active')){
+                link1.classList.remove('active');
+            }
+        })
+        link.classList.add("active");
+    })
+ })
+
+ //JAK ZROBIC ZEBY ZMIENIALO SIE ZDJECIE GLOWNE KIEDY SIE KLIKNIE MALE?
